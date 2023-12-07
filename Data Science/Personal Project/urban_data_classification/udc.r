0dbf4d4df782deb525c6808f0ed17ac331508415
xrander <olamideadu@gmail.com>
2023-12-07 12:25:26 +0100

    Nearest model hypertuned and tidymodels employed alone

--- Data Science/Personal Project/urban_data_classification/udc.r
+++ Data Science/Personal Project/urban_data_classification/udc.r
@@ -1,50 +1,44 @@
 # Set work direcetory
-
-#setwd("~/Documents/Data Science/Personal Project/urban_data_classification")
+setwd("~/Documents/Data Science/Personal Project/urban_data_classification")
 
 # Load library
-library("tidyverse") # for data manipulation and visualization
-library("tidymodels") # for splitting dat
-library("class") # library for classification
-library("caret") # for hyperparameter tuning
+library("tidyverse")
+library("janitor")
+library("tidymodels")
 
-# set work directory
-setwd("~/Documents/Data Science/Personal Project/urban_data_classification/")
+test_data <- read_csv("testing.csv")
+train_data <- read_csv("training.csv")
 
-# Read files from local disk
-tst <- read_csv("testing.csv") # test data
-trn <- read_csv("training.csv") # training data
+glimpse(test_data)
 
-# Missing data
-sum(is.na(tst))
-sum(is.na(trn)) # No missing data in the data frames
+sum(is.na(test_data))
+sum(is.na(train_data))
 
-# get the number of observation
-nrow(trn) # 168 observation
-nrow(tst) # 507
+nrow(train_data) # 168 observation
+nrow(test_data) # 507
 
 # EDA
-tst %>%
-  count(class)# There are 9 classes
-
-trn %>% 
+test_data %>%
   count(class)
 
-# combine both data to a whole
-# First we check if they have the same number and names of variables
+train_data %>% 
+  count(class)
 
-ncol(trn) == ncol(tst) # Test for equal number of variables
+compare_df_cols_same(test_data, train_data)
 
-unique(names(trn) ==  names(tst)) # Test for similar variables names
+urban_data <- bind_rows(train_data, test_data) %>% 
+  mutate(id = row_number()) %>%
+  select(id, everything()) %>% 
+  clean_names() %>% 
+  mutate_if(is.character, factor)
 
-urban_data <- bind_rows(trn, tst) # combine the data
+get_dupes(urban_data)
 
-nrow(urban_data) == sum(nrow(trn), nrow(tst)) # check if observations are properly combined
+urban_data <- urban_data %>%
+  select(-id)
 
 write_csv(urban_data, "urban_data.csv")
 
-# Target variable - class
-# Explanatory variable - Everything else
 
 # Distribution of the target variable
 urban_data %>%
@@ -57,17 +51,8 @@ urban_data %>%
   ggtitle("Frequency of Classes")+
   geom_text(aes(label = frequency,
                 vjust = 0.001))
-# The classes are not equal
 
-# normalize the numerical variable in the data
-urban_data <- urban_data %>%
-  mutate(class = factor(class,
-                        labels = c("car", "concrete", "tree", "building",
-                                   "asphalt", "grass","shadow", "soil","pool"),
-                        levels = c("car", "concrete", "tree", "building",
-                                   "asphalt", "grass","shadow", "soil","pool"),
-                        ordered = F), # change class to factor
-         across(where(is.double), scale))# scale the predictor
+
 
 # Set seed to ensure reproducible result
 set.seed(120)
@@ -81,30 +66,53 @@ urban_data_split <- initial_split(urban_data,
 urban_train <- training(urban_data_split)
 urban_test <- testing(urban_data_split)
 
+urban_train_resample <- vfold_cv(urban_train, v = 10)
+
+urban_trurban_train_rec <-
+  recipe(class ~., data = urban_train) %>% 
+  step_normalize(all_numeric_predictors())
+
 # Start work flow
 knn_workflow <- workflow()
 
 # Define nearest neighbour model
-knn_model <- nearest_neighbor(
-  neighbors = 10,
-  dist_power = 2) %>%
-  set_engine("kknn") %>%
-  set_mode("classification")
+knn_model <- nearest_neighbor(neighbors = tune(),
+                              dist_power = 2,
+                              engine = "kknn",
+                              mode = "classification")
+
+
 
 # add model to workflow
 knn_workflow <- knn_workflow %>%
-  add_model(knn_model)
+  add_model(knn_model) 
 
-# add target and feature variables
-knn_workflow <- knn_workflow %>%
-  add_variables(outcomes = class,
-                predictors = everything())
+knn_workflow <- knn_workflow %>% 
+  add_recipe(urban_train_rec)
 
-# preview workflow
-knn_workflow
+
+knn_grid <- tune_grid(
+  knn_workflow,
+  resamples = urban_train_resample
+)
+
+knn_grid %>%
+  collect_metrics()
+
+knn_grid %>% 
+  show_best("accuracy")
+
+knn_grid %>% 
+  show_best("roc_auc")
+
+
+# random_forest
+rand_forest()
+
 
 # fit the model
-knn_fit <- fit(knn_workflow, urban_train)
+knn_fit <- fit_resamples(knn_workflow,
+                         urban_train_resample)
 
 
 # Resampling
@@ -119,23 +127,7 @@ urban_test %>%
   count(class)%>%
   pull(n)
 
-# split each class in the data
-#urban_data_split<- split(urban_data, urban_data$class)
 
-# make rsplit data for each class
-
-#for (name in names(urban_data_split)){
-#  assign(paste0(name, "_rsplit"), initial_split(urban_data_split[[name]], prop = 0.8))
-#}
-
-
-# Decide k using hyperparameter tuning result
-#control <- trainControl(method = "cv", number = 10)
-
-# square root of num of observation in training data
-#round(sqrt(nrow(training_data)), 0) # to be included as part of k-values to choose from
-
-# Create k values to choose from in a grid
 grid <- expand_grid(k = seq(2, 25, 1))
 
 knn_model <- nearest_neighbor(
@@ -149,12 +141,7 @@ urban_train_fit <- knn_model %>%
 
 urban_train_fit
 
-knn_value <- train(
-  class ~ .,
-  data = urban_train,
-  method = "knn",
-  tuneGrid = grid,
-  trControl = trainControl(method = "cv", number = 10))
+
 
 print(knn_value) #k = 13
 
@@ -187,20 +174,17 @@ tibble("x" = testing_data$BrdIndx,
   geom_point(size = 2)+
   geom_smooth(method = lm, se = F, alpha = 0.4)+
   facet_wrap(~type)
-  
 
+#########################################################
 
-library(gmodels)
+rf_spec <- rand_forest(mode = "classification",
+                       mtry = tune(),
+                       trees = 1000,
+                       engine = "randomForest",
+                       min_n = tune())
 
-CrossTable(x = testing_data$class, y = urban_class_pred,
-           prop.chisq = F)
-
-# Load the xgboost library
-library(xgboost)
-# Creat xgboost data structure for training and test data
-xgb_trn_data <- xgb.DMatrix(data = as.matrix(training_data[, -1]), label = as.matrix(training_data$class)) 
-
-
-xgb.DMatrix(data = as.matrix(testing_data[, -1]))
-
-
+tune_grid(
+  class ~ .,
+  model = rf_spec,
+  resamples = 
+)
